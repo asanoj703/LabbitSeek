5be5dd1fe3c7bf84f10afc67bf8a259b7bcf47c4
asano <ninomiya>
2019-05-14 17:55:15 +0900

    画面遷移の実装

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Transform sprite;
+    float delta_x;
+
+    void Awake()
+    {
+        sprite = GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        delta_x = -0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScrollBG();
+    }
+
+    void ScrollBG()
+    {
+        sprite.position = new Vector3(sprite.position.x + delta_x, sprite.position.y, sprite.position.z);
+    }
+}
---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    [SerializeField]private string str;
+    [SerializeField]private Canvas canvas;
+    [SerializeField]private Scene unload_scene;
+
+    private void Start()
+    {
+    }
+
+    public void Transtion()
+    {
+        if (str != null)
+        {
+            StartCoroutine(LoadAsync());
+        } else
+        {
+            Debug.Log("シーン名を記入してください。");
+        }
+    }
+
+    IEnumerator LoadAsync()
+    {
+        //現在のシーンをunloadに設定
+        unload_scene = SceneManager.GetActiveScene();
+        //次のシーンをロードして現在のシーンをアンロードする
+        yield return SceneManager.LoadSceneAsync(str, LoadSceneMode.Additive);
+        yield return SceneManager.UnloadSceneAsync(unload_scene);
+    }
+
+    public void CoverCanvas()
+    {
+        if (canvas != null)
+        {
+            canvas.enabled = true;
+            StartCoroutine(Wait(2.0f));
+            //Transtion();
+        } else
+        {
+            Debug.Log("キャンバスを設定してください。");
+        }
+    }
+
+    IEnumerator Wait(float f)
+    {
+        yield return new WaitForSeconds(f);
+        Transtion();
+    }
+}
